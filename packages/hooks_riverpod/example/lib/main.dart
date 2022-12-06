480068bc23896029a9f9c60ca89c66238d69b342
jeiea <solum5013@gmail.com>
2022-12-06 18:51:20 +0900

    stash: for test

--- packages/hooks_riverpod/example/lib/main.dart
+++ packages/hooks_riverpod/example/lib/main.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+void main() {
+  runApp(const MyHomePage());
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('demo'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('demo'),
+        ),
+        body: const _Body(),
       ),
-      body: const _Body(),
     );
   }
 }
